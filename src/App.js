ed2e7fa442ca609bb1aa5225aa2f971d02fbe56d
hatykuxordik <hatykuxordik@gmail.com>
2023-10-10 01:10:35 +0100

    Update Handling Loading, Error, and Ready Status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -13,7 +16,7 @@ const reducer = function (state, action) {
       return {
         ...state,
         questions: action.payload,
-        status: "ready",
+        status: "Ready",
       };
 
     case "dataFailed":
@@ -28,11 +31,13 @@ const reducer = function (state, action) {
 };
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch(`http://localhost:8000/questions`)
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
@@ -41,7 +46,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
+        {status === "Loading" && <Loader />}
+        {status === "Error" && <Error />}
+        {status === "Ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
